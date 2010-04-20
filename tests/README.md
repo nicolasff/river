b652d4e9c6cb5f98c3bcf1de20e5dfa0e5b4ccca
Nicolas Favre-Felix <n.favrefelix@gmail.com>
2010-04-20 23:40:57 +0200

    Benchmark stub.

---
+++ tests/README.md
@@ -0,0 +1,1 @@
+This directory contains C programs that test the comet server.
---
+++ tests/bench.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+#include <event.h>
+
+
+
+int
+main(int argc, char *argv[]) {
+	(void)argc;
+	(void)argv;
+
+
+
+	return EXIT_SUCCESS;
+}
+
